ee58a3e91291f39a4936ee366c704484da96b6f3
zhao <zhao_zzq2012@163.com>
2020-12-02 14:51:59 +0800

    兼容 mPaaS容器

--- imaker.js
+++ imaker.js
@@ -10,7 +10,6 @@
   ///////////////////////////////////////////////////////////////////////////////////////////////// 
   ///////////////////////////////////本地调用的实际逻辑////////////////////////////////////////////
   var callbacksCount = 1, callbacks = {};
-
   function _handleMessageFromApp(callbackId, message) {
 
     try {
@@ -31,19 +30,33 @@
     return userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) ? 'ios' : userAgent.match(/Android/i) ? 'android' : '';
   }
 
+  function isNebula() {
+    var userAgent = navigator.userAgent;
+    return userAgent.match(/Nebula/i) ? true : false;
+  }
   // Use this in javascript to request native objective-c code
   // functionName : string (I think the name is explicit :p)
   // message : map of arguments // message 必须有值，没有传空对象{}
   // callback : function with n-arguments that is going to be called when the native code returned
   function _call(functionName, message, callback) {
     //只有在手机或电脑端云之家中才允许调用Xuntong JSBridge
+    if (message == undefined) {
+      message = {};
+    }
+
+    if (isNebula()) {
+      AlipayJSBridge.call(functionName, message, callback);
+      return
+    }
+
     if (!getOS()) return false;
     var hasCallback = callback && typeof callback == "function";
     var callbackId = hasCallback ? callbacksCount++ : 0;
 
-    if (hasCallback)
+    if (hasCallback) {
       callbacks[callbackId] = callback;
-
+    }
+    
     if (getOS() == 'ios') {
       window.webkit.messageHandlers.callAppMessageHandler.postMessage({
         "function": functionName,
@@ -56,32 +69,49 @@
   }
 
   function _pushWindow(url, title, hiddenNavBar) {
-    if (getOS() == 'ios') {
-      window.webkit.messageHandlers.pushWindow.postMessage({
-        "url": url,
-        "title": title,
-        "hiddenNavBar": hiddenNavBar
-      });
-    } else if (getOS == 'android') {
-      imaker.pushWindow(url, title, hiddenNavBar); //android 需实现imaker
-    }
+    _call("pushWindow", {
+      "url": url,
+      "title": title,
+      "hiddenNavBar": hiddenNavBar
+    });
   }
 
   function _closeWindow() {
-    if (getOS() == 'ios') {
-      window.webkit.messageHandlers.closeWindow.postMessage({});
-    } else if (getOS == 'android') {
-      imaker.closeWindow(functionName, message, callbackId); //android 需实现imaker
-    }
+    _call("closeWindow",{});
   }
+
+  function _scanQRCode(callback) {
+    if (isNebula()) {
+      AlipayJSBridge.call('scan', {
+        type: 'qr',
+      }, function(result) {
+        let error = result.error;
+        var data = {
+          code: error,
+          data: result.qrCode ? result.qrCode: result.barCode
+        }
+        if (error == 10 || error == 11) {
+          data.desc = error == 10 ? "用户取消" : "操作失败";
+        } else {
+          data.code = 0;
+          data.desc = "";
+        }
+        callback(data);
+      });
+      return;
+    }
+    _call("scanQRCode",{},callback);
+  }
+
 
   var __imakerapi = {
     invoke: _call,
     call: _call,
     callbackHandler: _handleMessageFromApp,
-    //convenience
+    //convenience function
     closeWindow: _closeWindow,
-    pushWindow: _pushWindow
+    pushWindow: _pushWindow,
+    scanQRCode: _scanQRCode,
   };
 
   window.imakerapi = __imakerapi;
